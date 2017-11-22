e78550c1e08cd1c70c53c93c1ded1d4818d111b6
vectronic <hello.vectronic@gmail.com>
2017-11-22 17:28:38 +0000

    still debugging

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ module.exports = function(homebridge) {
 };
 
 
+// The maximum is exclusive and the minimum is inclusive
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 function PingHostsPlatform(log, config) {
 	this.log = log;
     this.hosts = config["hosts"] || [];
@@ -21,6 +29,9 @@ function PingHostsPlatform(log, config) {
 
 PingHostsPlatform.prototype.accessories = function (callback) {
     var accessories = [];
+    if (this.hosts.length > 100) {
+        throw new Error("Max 100 hosts supported, might run into ping session ID problems otherwise....");
+    }
     for (var i = 0; i < this.hosts.length; i++) {
         accessories.push(new PingHostContactAccessory(this.log, this.hosts[i], i+1));
     }
@@ -58,32 +69,37 @@ function PingHostContactAccessory(log, config, id) {
         .setValue(Characteristic.ContactSensorState.CONTACT_DETECTED);
 
     this.options = {
-        sessionId: id,
         networkProtocol: ping.NetworkProtocol.IPv4,
-        retries: config["retries"] || 2,
-        timeout: (config["timeout"] || 20) * 1000
+        retries: 1,
+        timeout: (config["timeout"] || 30) * 1000
     };
 
 	setInterval(this.doPing.bind(this), (config["interval"] || 60) * 1000);
 }
 
-
 PingHostContactAccessory.prototype.doPing = function () {
+
+    // Random session IDs from a block of 100 per host ID. Make sure never 0.
+    this.options.sessionId = getRandomInt((this.id + 1) * 100, (this.id + 2) * 100);
 
     var session = ping.createSession(this.options);
 
     var self = this;
 
     session.on("error", function (error) {
-        self.log("[" + self.name + "] socket error: " + error.toString());
+        self.log("[" + self.name + "] socket error with session " + self.options.sessionId +  ": " + error.toString());
         self.services.ContactSensor
             .getCharacteristic(Characteristic.ContactSensorState)
             .updateValue(Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
     });
 
-    session.pingHost(self.host, function (error) {
+    session.on("close", function () {
+        self.log("[" + self.name + "] socket with session " + self.options.sessionId + " closed");
+    });
+
+    session.pingHost(self.host, function (error, target, sent, rcvd) {
         if (error) {
-            self.log("[" + self.name + "] response error:" + error.toString());
+            self.log("[" + self.name + "] response error: " + error.toString() + " for " + target + " at " + sent + " with session " + self.options.sessionId);
             self.services.ContactSensor
                 .getCharacteristic(Characteristic.ContactSensorState)
                 .updateValue(Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
